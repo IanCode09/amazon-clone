1e783a7a75e60c54498a863c77d3be2d75274728
IanCode09 <ianlombu@protonmail.com>
2020-11-04 13:43:16 +0700

    Initial commit + header_search

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCTOm-EAfkQQmoeWvMG23WEnmdu03n7vbQ",
+  authDomain: "challenge-a86e9.firebaseapp.com",
+  databaseURL: "https://challenge-a86e9.firebaseio.com",
+  projectId: "challenge-a86e9",
+  storageBucket: "challenge-a86e9.appspot.com",
+  messagingSenderId: "94998083123",
+  appId: "1:94998083123:web:94603f99049361a2a6d546",
+  measurementId: "G-BLPQY1RYQE"
+};
