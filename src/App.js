362c9f7f71439d5246b7f2bc96654d596f5ad850
IanCode09 <ianlombu@protonmail.com>
2020-11-06 00:18:47 +0700

    Checkout Component Part-1 = Component Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css';
 import Header from './components/Header'
-import Home from './components/Home'
+import Home from './screens/Home'
+import Checkout from './components/Checkout'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
@@ -10,14 +11,14 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
+        
         <Switch>
           <Route path="/" exact>
-            <Header />
             <Home />
           </Route>
           <Route path="/checkout">
-            <Header />
-            <h1>You Did IT</h1>
+            <Checkout />
           </Route>
         </Switch>
       </div>
