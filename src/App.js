295ca06899c6d4d1a2660f1a83fb5db09e5ecb2e
IanCode09 <ianlombu@protonmail.com>
2020-11-06 15:28:39 +0700

    Login Screen

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 import Header from './components/Header'
 import Home from './screens/Home'
 import Checkout from './screens/Checkout'
+import Login from './screens/Login'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
-        
         <Switch>
           <Route path="/" exact>
+            <Header />
             <Home />
           </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
+          </Route>
+          <Route path='/login'>
+            <Login />
           </Route>
         </Switch>
       </div>
