fb3f7f0c0afcb5ba2ea58e064f010d8e3cd0ca7e
IanCode09 <ianlombu@protonmail.com>
2020-11-06 16:03:04 +0700

    Register & Login Functionality

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyCTOm-EAfkQQmoeWvMG23WEnmdu03n7vbQ",
   authDomain: "challenge-a86e9.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:94998083123:web:94603f99049361a2a6d546",
   measurementId: "G-BLPQY1RYQE"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { db, auth }
